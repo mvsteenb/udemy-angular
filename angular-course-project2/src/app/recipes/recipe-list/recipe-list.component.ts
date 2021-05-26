5ab76776da3f30f06f89ed566041f91c07070ff6
Mario Van Steenberghe <m.vansteenberghe@Medix.local>
2021-05-26 19:00:10 +0200

    implementing recipe item selection + detail

--- angular-course-project2/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-course-project2/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../model/recipe.model";
 
 @Component({
@@ -10,10 +10,16 @@ export class RecipeListComponent implements OnInit{
   
   recipes: Recipe[] = [
     new Recipe('Ricechicken', 'Rice & chicken', 'https://www.simplyrecipes.com/thmb/Mrdls0tDdLyQ-cWAKPyOSpMXuds=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Chilaquiles-LEAD-1-be30b6674d3b43288bebc87e5eca1bec.jpg'),
-    new Recipe('Ricechicken', 'Rice & chicken', 'https://www.simplyrecipes.com/thmb/Mrdls0tDdLyQ-cWAKPyOSpMXuds=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Chilaquiles-LEAD-1-be30b6674d3b43288bebc87e5eca1bec.jpg')
+    new Recipe('Spaghetti', 'Spaghetti Bolognese', 'https://static.ah.nl/static/recepten/img_004328_1600x_JPG.jpg')
   ];
+
+  @Output('onRecipeSelected') recipeSelectedEventEmitter = new EventEmitter<Recipe>();
 
   ngOnInit() {
 
+  }
+
+  setSelectedRecipe(recipe: Recipe) {
+    this.recipeSelectedEventEmitter.emit(recipe);
   }
 }
