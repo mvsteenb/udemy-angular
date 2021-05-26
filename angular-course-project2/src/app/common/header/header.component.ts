5eb97bed07f5b84fe8780c8ae28fd44eae138c64
Mario Van Steenberghe <m.vansteenberghe@Medix.local>
2021-05-26 18:39:57 +0200

    implementing menu selection

--- angular-course-project2/src/app/common/header/header.component.ts
+++ angular-course-project2/src/app/common/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  selectedComponent = 'recipes';
+  @Output('componentChangeEvent') componentChangeEventEmitter = new EventEmitter<{component: string}>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Sets selected component
+   * @param componentName component name
+   */
+
+  setSelectedComponent(componentName: string) {
+    console.log("selected component: " + componentName);
+    this.selectedComponent = componentName;
+    this.componentChangeEventEmitter.emit({component : this.selectedComponent});
   }
 
 }
