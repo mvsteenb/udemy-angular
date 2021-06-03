ea2019ca498400f556c885f832a8603c8a7e1118
Mario Van Steenberghe <m.vansteenberghe@Medix.local>
2021-06-03 23:51:37 +0200

    implementig forms task

---
+++ task-forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  subscriptions = [
+    'Basic',
+    'Advanced',
+    'Pro'
+  ]
+
+  defaultSubscription = 'Advanced';
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+
+}
