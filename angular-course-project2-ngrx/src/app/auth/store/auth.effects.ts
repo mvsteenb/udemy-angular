ac455ab67105670555234654f473e13957f5ae3e
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2021-06-11 23:45:24 +0200

    implementing ngrx

---
+++ angular-course-project2-ngrx/src/app/auth/store/auth.effects.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "@angular/common/http";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { of } from "rxjs";
+import { catchError, map, switchMap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import * as AuthActions from './auth.actions';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+// effect should return a new action in the end
+
+export class AuthEffects {
+
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.fireBaseAPIKey,
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true
+        }
+      ).pipe(
+        catchError(
+          error => {
+            of();
+          }
+        ),
+        map(
+          resData => {
+            of();
+          }
+        )
+      )
+    }),
+
+  );
+  
+  constructor(private actions$: Actions, private http: HttpClient) {}
+
+  
+}
